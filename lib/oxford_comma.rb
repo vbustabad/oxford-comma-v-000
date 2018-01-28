5755c5e749ada26d392e7077d69beb26bf87b5c4
Veronica Bustabad <vbustabad@gmail.com>
2018-01-28 00:55:50 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
   elsif array.size == 3
     array.join(", ").insert(14, "and ")
   elsif array.size > 3 && array.size < 6
-    array.join(", ").insert(33, "and ") 
+    array.join(", ").insert(33, "and ")
   else array.size > 5 && array.size < 8
     array.join(", ").insert(57, "and ")
   end
